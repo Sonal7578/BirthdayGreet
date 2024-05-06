b2f880b19514573309649c9ecc648bdb0e4c4e6d
Sonal <sonal.dhage26@gmail.com>
2024-05-06 16:36:33 +0200

    Edited the toast message based on the entered name

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.birthdaygreet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameInput: EditText // Declare EditText variable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize nameInput by finding the view with id 'nameInput'
+        nameInput = findViewById(R.id.nameInput)
     }
 
     fun createBirthdayCard(view: View) {
-        Toast.makeText(this, "Button was clicked", Toast.LENGTH_LONG).show()
+
+        val name = nameInput.editableText.toString()
+
+        Toast.makeText(this, "Name is $name", Toast.LENGTH_LONG).show()
     }
 }
